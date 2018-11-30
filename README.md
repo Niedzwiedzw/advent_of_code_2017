f6237c7fbb17fb6f5306a5a7450563d1935da887
niedzwiedzwo@gmail.com <niedzwiedzwo@gmail.com>
2018-11-30 20:52:24 +0100

    Day 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of code 2017
+To run specific example you need `Rust Programming Language` (https://rustup.rs/).
+
+To run specific day run 
+
+```bash
+cargo run --example day#  # substitute "#" with a proper day.
+```
+
---
+++ examples/day1.rs
@@ -0,0 +1,31 @@
+// https://adventofcode.com/2017/day/1
+use std::fs::File;
+use std::io::{ Result, Read };
+
+
+fn get_digits() -> Vec<u32> {
+    let mut file = File::open("data/day1.txt").expect("File is provided.");
+    let mut content = Vec::new();
+
+    file.read_to_end(&mut content).expect("File is provided");
+    content.iter()
+        .map(|b| *b as char)
+        .map(|d| d.to_digit(10 as u32).unwrap())
+        .collect()
+}
+
+fn main() -> Result<()> {
+    let digits = get_digits();
+    let digit_cycle = digits.iter().chain(
+        digits.iter().take(1)
+    );
+    let mut sum = 0;
+    for (first, other) in digit_cycle.clone().zip(digit_cycle.skip(1)){
+        if first == other {
+            sum += first;
+        }
+    }
+
+    println!("{}", sum);
+    Ok(())
+}
